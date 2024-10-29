ac38ba4ecaee0e014f7f05b035b0057e193bbc52
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-10-29 14:38:04 +0530

    reset

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Vercel! The Express app is running successfully.");
+    res.json({ message: "Hello, Vercel! The Express app is running successfully." });
 });
 
 app.get("/test", (req, res) => {
     res.json({ message: "This is a test route on Vercel!" });
+});
+
+app.use((err, req, res) => {
+    res.status(404).json({ message: "Route not found", error: err.message });
 });
 
 export default app;
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import app from "./app.js";
 
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_DB_NAME}`);
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+// })();
 
-export default app;
+export default function handler(req, res) {
+    app(req, res);
+}
